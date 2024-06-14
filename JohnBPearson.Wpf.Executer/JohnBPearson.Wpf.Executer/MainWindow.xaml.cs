9de85ff9115cc930ee5982f630bd4ab96897a2e0
John Pearson <104274223+johnbp2@users.noreply.github.com>
2024-06-13 21:02:22 -0500

    first prototype animation

--- JohnBPearson.Wpf.Executer/JohnBPearson.Wpf.Executer/MainWindow.xaml.cs
+++ JohnBPearson.Wpf.Executer/JohnBPearson.Wpf.Executer/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using JohnBPearson.Butlers.QuickLaunchCore;
 using JohnBPearson.Butlers.QuickLaunchCore.FileMetaDataModel;
 using System.Runtime.InteropServices;
+using System.Windows.Media.Animation;
 
 namespace JohnBPearson.Wpf.Executer
 {
@@ -62,10 +63,14 @@ namespace JohnBPearson.Wpf.Executer
         private const int iamageWidth = 32;
         private void Main_Initialized(object sender, EventArgs e)
         {
+#if DEBUG
+            this.implementAnimatedImages();
+#endif
 
-          
+#if !DEBUG
             displayLinks();
-
+#endif
+         //   buildAnimatedImagesExample();
         }
         private void setOnTop()
         {
@@ -152,6 +157,282 @@ namespace JohnBPearson.Wpf.Executer
             setOnTop();
 
         }
+        private Storyboard? _mystoryboard = null;
+
+        private void implementAnimatedImages()
+        {
+            NameScope.SetNameScope(this, new NameScope());
+
+            //this.WindowTitle = "Animate Properties using Storyboards";
+           // StackPanel stack1 = new StackPanel();
+          //  stack1.MinWidth = 500;
+           // stack1.Margin = new Thickness(30);
+            stack1.HorizontalAlignment = HorizontalAlignment.Left;
+            //   TextBlock myTextBlock = new TextBlock();
+            //  myTextBlock.Text = "Storyboard Animation Example";
+            // stack1.Children.Add(myTextBlock);
+
+            //
+            // Create and animate the first image.
+            //
+
+            // Create a images.
+            Facade facade = new Butlers.QuickLaunchCore.Facade(Properties.Settings.Default.folder);
+            var i = 100;
+            var margin = 0;
+            var width = 0;
+
+            foreach(var fileSystemObject in facade.FileSystemObjects)
+            {
+
+                if(fileSystemObject.Type != Extension.ini)
+                {
+                    var image2 = new Image();
+                    image2.Name = $"image{i}";
+                    this.RegisterName(image2.Name, image2);
+
+                    image2.Width = iamageWidth;
+                    width = width + iamageWidth + 10;
+
+                    var bmi = imageService.IconToBitmapImage(fileSystemObject.Icon);
+                    
+                    image2.BeginInit();
+                    image2.Source = bmi;
+                    image2.EndInit();
+                 
+                    image2.MouseDown += new MouseButtonEventHandler(Mouse_Down);
+                    image2.Margin = new Thickness(0, 0, 10, 0);
+                    cache.Add(new Tuple<Image, IFileSystemObjectBase>(image2, fileSystemObject));
+                   
+                   // image2.ToolTip = fileSystemObject.Name;
+
+
+                    this.RegisterName(image2.Name, image2);
+
+
+
+                    DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+                    myDoubleAnimation.From = 0.0;
+                    myDoubleAnimation.To = 300;
+
+                    myDoubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(5));
+                    myDoubleAnimation.AutoReverse = true;
+
+                    Storyboard.SetTargetName(myDoubleAnimation, image2.Name);
+                    Storyboard.SetTargetProperty(myDoubleAnimation, new PropertyPath(Image.));
+                   this._mystoryboard = new Storyboard();
+                    this._mystoryboard.Children.Add(myDoubleAnimation);
+                    image2.MouseEnter += new MouseEventHandler(Image2_MouseEnter);
+
+
+
+
+
+
+                    this.stack1.Children.Add(image2);
+                   
+                    i++;
+                   
+                }
+               
+            }
+            //Button myWidthAnimatedButton = new Button();
+            //myWidthAnimatedButton.Height = 30;
+            //myWidthAnimatedButton.Width = 200;
+            //myWidthAnimatedButton.HorizontalAlignment = HorizontalAlignment.Left;
+            //myWidthAnimatedButton.Content = "A Button";
+
+            // Set the Name of the button so that it can be referred
+            // to in the storyboard that's created later.
+            // The ID doesn't have to match the variable name;
+            // it can be any unique identifier.
+           // myWidthAnimatedButton.Name = "myWidthAnimatedButton";
+
+            // Register the name with the page to which the button belongs.
+           // this.RegisterName(myWidthAnimatedButton.Name, myWidthAnimatedButton);
+
+            // Create a DoubleAnimation to animate the width of the button.
+            //DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+            //myDoubleAnimation.From = 200;
+            //myDoubleAnimation.To = 300;
+            //myDoubleAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(3000));
+
+            // Configure the animation to target the button's Width property.
+            //Storyboard.SetTargetName(myDoubleAnimation, myWidthAnimatedButton.Name);
+            //Storyboard.SetTargetProperty(myDoubleAnimation, new PropertyPath(Button.WidthProperty));
+
+            // Create a storyboard to contain the animation.
+            //Storyboard myWidthAnimatedButtonStoryboard = new Storyboard();
+            //myWidthAnimatedButtonStoryboard.Children.Add(myDoubleAnimation);
+
+            // Animate the button width when it's clicked.
+            
+
+            //
+            // Create and animate the second button.
+            //
+
+            // Create a second button.
+            //Button myColorAnimatedButton = new Button();
+            //myColorAnimatedButton.Height = 30;
+            //myColorAnimatedButton.Width = 200;
+            //myColorAnimatedButton.HorizontalAlignment = HorizontalAlignment.Left;
+            //myColorAnimatedButton.Content = "Another Button";
+
+            //// Create a SolidColorBrush to paint the button's background.
+            //SolidColorBrush myBackgroundBrush = new SolidColorBrush();
+            //myBackgroundBrush.Color = Colors.Blue;
+
+            //// Because a Brush isn't a FrameworkElement, it doesn't
+            //// have a Name property to set. Instead, you just
+            //// register a name for the SolidColorBrush with
+            //// the page where it's used.
+            //this.RegisterName("myAnimatedBrush", myBackgroundBrush);
+
+            //// Use the brush to paint the background of the button.
+            //myColorAnimatedButton.Background = myBackgroundBrush;
+
+            //// Create a ColorAnimation to animate the button's background.
+            //ColorAnimation myColorAnimation = new ColorAnimation();
+            //myColorAnimation.From = Colors.Red;
+            //myColorAnimation.To = Colors.Blue;
+            //myColorAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(7000));
+
+            //// Configure the animation to target the brush's Color property.
+            //Storyboard.SetTargetName(myColorAnimation, "myAnimatedBrush");
+            //Storyboard.SetTargetProperty(myColorAnimation, new PropertyPath(SolidColorBrush.ColorProperty));
+
+            //// Create a storyboard to contain the animation.
+            //Storyboard myColorAnimatedButtonStoryboard = new Storyboard();
+            //myColorAnimatedButtonStoryboard.Children.Add(myColorAnimation);
+
+            //// Animate the button background color when it's clicked.
+            //myColorAnimatedButton.Click += delegate (object sender, RoutedEventArgs args)
+            //{
+            //    myColorAnimatedButtonStoryboard.Begin(myColorAnimatedButton);
+            //};
+
+            //stack1.Children.Add(image2);
+            //this.Content = stack1;
+
+        }
+
+
+        public void Image2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(this._mystoryboard != null)
+            { 
+                this._mystoryboard.Begin(sender as Image);
+            }
+        }
+
+        //private void buildAnimatedImagesExample()
+        //{
+        //    NameScope.SetNameScope(this, new NameScope());
+
+        //    //this.WindowTitle = "Animate Properties using Storyboards";
+        //    StackPanel myStackPanel = new StackPanel();
+        //    myStackPanel.MinWidth = 500;
+        //    myStackPanel.Margin = new Thickness(30);
+        //    myStackPanel.HorizontalAlignment = HorizontalAlignment.Left;
+        //    TextBlock myTextBlock = new TextBlock();
+        //    myTextBlock.Text = "Storyboard Animation Example";
+        //    myStackPanel.Children.Add(myTextBlock);
+
+        //    //
+        //    // Create and animate the first button.
+        //    //
+
+        //    // Create a button.
+        //    Button myWidthAnimatedButton = new Button();
+        //    myWidthAnimatedButton.Height = 30;
+        //    myWidthAnimatedButton.Width = 200;
+        //    myWidthAnimatedButton.HorizontalAlignment = HorizontalAlignment.Left;
+        //    myWidthAnimatedButton.Content = "A Button";
+
+        //    // Set the Name of the button so that it can be referred
+        //    // to in the storyboard that's created later.
+        //    // The ID doesn't have to match the variable name;
+        //    // it can be any unique identifier.
+        //    myWidthAnimatedButton.Name = "myWidthAnimatedButton";
+
+        //    // Register the name with the page to which the button belongs.
+        //    this.RegisterName(myWidthAnimatedButton.Name, myWidthAnimatedButton);
+
+        //    // Create a DoubleAnimation to animate the width of the button.
+        //    DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+        //    myDoubleAnimation.From = 200;
+        //    myDoubleAnimation.To = 300;
+        //    myDoubleAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(3000));
+
+        //    // Configure the animation to target the button's Width property.
+        //    Storyboard.SetTargetName(myDoubleAnimation, myWidthAnimatedButton.Name);
+        //    Storyboard.SetTargetProperty(myDoubleAnimation, new PropertyPath(Button.WidthProperty));
+
+        //    // Create a storyboard to contain the animation.
+        //    Storyboard myWidthAnimatedButtonStoryboard = new Storyboard();
+        //    myWidthAnimatedButtonStoryboard.Children.Add(myDoubleAnimation);
+
+        //    // Animate the button width when it's clicked.
+        //    myWidthAnimatedButton.Click += delegate (object sender, RoutedEventArgs args)
+        //    {
+        //        myWidthAnimatedButtonStoryboard.Begin(myWidthAnimatedButton);
+        //    };
+
+        //    myStackPanel.Children.Add(myWidthAnimatedButton);
+
+        //    //
+        //    // Create and animate the second button.
+        //    //
+
+        //    // Create a second button.
+        //    Button myColorAnimatedButton = new Button();
+        //    myColorAnimatedButton.Height = 30;
+        //    myColorAnimatedButton.Width = 200;
+        //    myColorAnimatedButton.HorizontalAlignment = HorizontalAlignment.Left;
+        //    myColorAnimatedButton.Content = "Another Button";
+
+        //    // Create a SolidColorBrush to paint the button's background.
+        //    SolidColorBrush myBackgroundBrush = new SolidColorBrush();
+        //    myBackgroundBrush.Color = Colors.Blue;
+
+        //    // Because a Brush isn't a FrameworkElement, it doesn't
+        //    // have a Name property to set. Instead, you just
+        //    // register a name for the SolidColorBrush with
+        //    // the page where it's used.
+        //    this.RegisterName("myAnimatedBrush", myBackgroundBrush);
+
+        //    // Use the brush to paint the background of the button.
+        //    myColorAnimatedButton.Background = myBackgroundBrush;
+
+        //    // Create a ColorAnimation to animate the button's background.
+        //    ColorAnimation myColorAnimation = new ColorAnimation();
+        //    myColorAnimation.From = Colors.Red;
+        //    myColorAnimation.To = Colors.Blue;
+        //    myColorAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(7000));
+
+        //    // Configure the animation to target the brush's Color property.
+        //    Storyboard.SetTargetName(myColorAnimation, "myAnimatedBrush");
+        //    Storyboard.SetTargetProperty(myColorAnimation, new PropertyPath(SolidColorBrush.ColorProperty));
+
+        //    // Create a storyboard to contain the animation.
+        //    Storyboard myColorAnimatedButtonStoryboard = new Storyboard();
+        //    myColorAnimatedButtonStoryboard.Children.Add(myColorAnimation);
+
+        //    // Animate the button background color when it's clicked.
+        //    myColorAnimatedButton.Click += delegate (object sender, RoutedEventArgs args)
+        //    {
+        //        myColorAnimatedButtonStoryboard.Begin(myColorAnimatedButton);
+        //    };
+
+        //    myStackPanel.Children.Add(myColorAnimatedButton);
+        //    this.Content = myStackPanel;
+
+
+
+
+
+        //}
     }
 
 
