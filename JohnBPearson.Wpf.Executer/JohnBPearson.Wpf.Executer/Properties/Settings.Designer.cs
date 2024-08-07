b7db9d84674673c4737663a5d06297eb3c707674
John Pearson <104274223+johnbp2@users.noreply.github.com>
2024-08-07 00:15:06 -0500

    fixed settings

--- JohnBPearson.Wpf.Executer/JohnBPearson.Wpf.Executer/Properties/Settings.Designer.cs
+++ JohnBPearson.Wpf.Executer/JohnBPearson.Wpf.Executer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace JohnBPearson.Wpf.Executer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -44,6 +44,30 @@ namespace JohnBPearson.Wpf.Executer.Properties {
             }
             set {
                 this["alwaysOnTop"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool horizontalOrientation {
+            get {
+                return ((bool)(this["horizontalOrientation"]));
+            }
+            set {
+                this["horizontalOrientation"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool animationOn {
+            get {
+                return ((bool)(this["animationOn"]));
+            }
+            set {
+                this["animationOn"] = value;
             }
         }
     }
