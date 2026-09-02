529bbef5e1993c40795b6fd84241e977589c4af6
John Pearson <104274223+johnbp2@users.noreply.github.com>
2026-09-02 11:47:44 -0500

    Add new settings, refactor docking, and cleanup shortcuts
    Introduced new user settings (`opacity`, `bgColor`, `dock`) to enhance customization. Updated the `folder` setting to initialize dynamically if empty. Refactored docking logic into a `DockWindow` method for better maintainability.
    
    Removed binary shortcut files and their references to streamline the project. Updated `Setup.vdproj` to include a new icon and adjust build configurations. Modified assembly metadata and incremented version to `0.0.3.0`.

--- JohnBPearson.Wpf.Executer/JohnBPearson.Wpf.Executer/Properties/AssemblyInfo.cs
+++ JohnBPearson.Wpf.Executer/JohnBPearson.Wpf.Executer/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyDescription(" ")]
-[assembly: AssemblyVersion("0.0.0.2")]
-[assembly: AssemblyFileVersion("0.0.0.2")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
